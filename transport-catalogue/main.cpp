cba2b83ace71b67d536256523b63f0aad5dd4808
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-04-14 10:56:22 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,52 +1,15 @@
-#include <iostream>
-#include <string>
-#include <cassert>
-#include <vector>
-
 #include "input_reader.h"
-#include "stat_reader.h"
+#include "transport_catalogue.h"
 
 using namespace std;
-
+using namespace transport_catalogue;
+using namespace query;
 
 int main() {
-
-	transport_catalogue::TransportCatalogue catalogue;
-	// РЕАЛИЗОВАНЫ 2 ВАРИАНТА НА ВЫЗОВ ЗАПОЛНЕНИЯ БАЗЫ ДАННЫХ:
-
-	// Вариант 1 (Поток ввода выведен в аргумент)
-	{
-		InputReader reader;
-		reader.ReadingStream(catalogue, reader, cin);
-	}
-
-
-
-	/*
-	// Вариант 2 (Поток ввода вне)
-	int base_request_count;
-	cin >> base_request_count >> ws;
-
-	{
-		InputReader reader;
-		reader.ReadingStream(catalogue, reader, base_request_count);
-
-	}
-	*/
-
-
-	//_________________________________________________
-	// РЕАЛИЗОВАНЫ 2 ВАРИАНТА ВЫЗОВА ЗАПРОСА СТАТИСТИКИ
-
-	// Вариант 1 (Поток ввода выведен в аргумент);
-	RequestStatistics(catalogue, cin);
-
-	/*
-	// Вариант 2 (Поток ввода вне)
-	int stat_request_count;
-	cin >> stat_request_count >> ws;
-
-	RequestStatistics(catalogue, cin, stat_request_count);
-	*/
-
+    TransportCatalogue tc;
+    InputReader ir;
+    ir.ParseInput();
+    ir.ParseInput();
+    ir.Load(tc);
+    return 0;
 }
