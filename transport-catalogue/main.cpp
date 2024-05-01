3470d71b466760c9901f573039f894415397a25c
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-01 14:44:29 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,15 +1,19 @@
-#include "input_reader.h"
-#include "transport_catalogue.h"
+#include "json_reader.h"
+#include "request_handler.h"
 
-using namespace std;
-using namespace transport_catalogue;
-using namespace query;
+#include <fstream>
 
 int main() {
-    TransportCatalogue tc;
-    InputReader ir;
-    ir.ParseInput();
-    ir.ParseInput();
-    ir.Load(tc);
-    return 0;
+	transport::Catalogue catalogue;
+
+	JsonReader json_doc(std::cin);
+
+	json_doc.FillCatalogue(catalogue);
+
+	const auto& stat_requests = json_doc.GetStatRequests();
+	const auto& render_settings = json_doc.GetRenderSettings().AsMap();
+	const auto& renderer = json_doc.FillRenderSettings(render_settings);
+
+	RequestHandler rh(catalogue, renderer);
+	json_doc.ProcessRequests(stat_requests, rh);
 }
