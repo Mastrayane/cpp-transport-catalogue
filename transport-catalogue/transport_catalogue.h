3b0560dff427bb400716a19f0e82ba2713946b53
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-03 18:22:51 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -27,15 +27,28 @@ namespace transport {
 		};
 
 		void AddStop(std::string_view stop_name, const geo::Coordinates coordinates);
-		void AddRoute(std::string_view bus_number, const std::vector<const Stop*> stops, bool is_circle);
+		void AddRoute(std::string_view bus_number, const std::vector<const Stop*>& stops, bool is_circle);
 		const Bus* FindRoute(std::string_view bus_number) const;
 		const Stop* FindStop(std::string_view stop_name) const;
-		size_t UniqueStopsCount(std::string_view bus_number) const;
+
+		size_t GetUniqueStopsCount(std::string_view bus_number) const;
+		/*
+		Извините, в request_handler.cpp уже есть метод GetBusStat,
+		который возвращает сразу все характеристики маршрута.
+		Данный метод GetBusStat использует метод GetUniqueStopsCount,
+		и перевод метода в приватную область делает его недоступным в class RequestHandler
+		даже в случае создания наследования RequestHandler от Catalogue или обратного.
+		Дублирование данного метода в этот класс, навереное не то, что вы от меня хотели.
+
+		Скорее всего я не правильно Вас понял. Уточните пожалуйста этот момент.
+
+		*/
 		void SetDistance(const Stop* from, const Stop* to, const int distance);
 		int GetDistance(const Stop* from, const Stop* to) const;
 		const std::map<std::string_view, const Bus*> GetSortedAllBuses() const;
 
 	private:
+
 		std::deque<Bus> all_buses_;
 		std::deque<Stop> all_stops_;
 		std::unordered_map<std::string_view, const Bus*> busname_to_bus_;
