81eac14f5a4a8eaede936cf531edcb882c11bdf3
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-18 16:28:16 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,53 +1,34 @@
 #pragma once
-
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
-#include "request_handler.h"
-
-#include <iostream>
-
-struct StopData {
-	std::string_view name;
-	geo::Coordinates coordinates;
-	std::map<std::string_view, int> distances;
-};
-
-struct RouteData {
-	std::string_view number;
-	std::vector<const transport::Stop*> stops;
-	bool circular_route;
-};
-
-class JsonReader {
+ 
+namespace transport_catalogue {
+namespace detail {
+namespace json {
+    
+class JSONReader{
 public:
-	explicit JsonReader(std::istream& input)
-		: input_(json::Load(input))
-	{}
-
-	const json::Node& GetBaseRequests() const;
-	const json::Node& GetStatRequests() const;
-	const json::Node& GetRenderSettings() const;
-
-	void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
-
-	void ProcessStopRequests(transport::Catalogue& catalogue);
-
-	void ProcessBusRequests(transport::Catalogue& catalogue);
-
-	void FillCatalogue(transport::Catalogue& catalogue);
-
-	renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
-
-	const json::Node PrintRoute(const json::Dict& request_map, RequestHandler& rh) const;
-	const json::Node PrintStop(const json::Dict& request_map, RequestHandler& rh) const;
-	const json::Node PrintMap(const json::Dict& request_map, RequestHandler& rh) const;
-
+    JSONReader() = default;    
+    JSONReader(Document doc);
+    JSONReader(std::istream& input);
+    
+    void parse_node_base(const Node& root, TransportCatalogue& catalogue);
+    void parse_node_stat(const Node& root, std::vector<StatRequest>& stat_request);
+    void parse_node_render(const Node& node, map_renderer::RenderSettings& render_settings);
+    void parse_node(const Node& root, TransportCatalogue& catalogue, std::vector<StatRequest>& stat_request, map_renderer::RenderSettings& render_settings);
+    void parse(TransportCatalogue& catalogue, std::vector<StatRequest>& stat_request, map_renderer::RenderSettings& render_settings);
+    
+    Stop parse_node_stop(Node& node);
+    Bus parse_node_bus(Node& node, TransportCatalogue& catalogue);
+    std::vector<Distance> parse_node_distances(Node& node, TransportCatalogue& catalogue);
+    
+    const Document& get_document() const;
+    
 private:
-	json::Document input_;
-	json::Node dummy_ = nullptr;
-
-	void FillStopDistances(transport::Catalogue& catalogue) const;
-	RouteData FillRoute(const json::Dict& request_map, transport::Catalogue& catalogue) const;
-	StopData FillStop(const json::Dict& request_map) const;
+    Document document_;
 };
+    
+}//end namespace json
+}//end namespace detail
+}//end namespace transport_catalogue
