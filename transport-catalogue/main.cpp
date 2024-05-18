f1e1f9ec17ffda219e6602634724d182b9b64daa
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-18 16:25:14 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,19 +1,25 @@
 #include "json_reader.h"
+#include "map_renderer.h"
 #include "request_handler.h"
-
-#include <fstream>
-
+ 
+using namespace std;
+using namespace transport_catalogue;
+using namespace map_renderer;
+using namespace request_handler;
+using namespace transport_catalogue::detail::json;
+ 
 int main() {
-	transport::Catalogue catalogue;
-
-	JsonReader json_doc(std::cin);
-
-	json_doc.FillCatalogue(catalogue);
-
-	const auto& stat_requests = json_doc.GetStatRequests();
-	const auto& render_settings = json_doc.GetRenderSettings().AsMap();
-	const auto& renderer = json_doc.FillRenderSettings(render_settings);
-
-	RequestHandler rh(catalogue, renderer);
-	json_doc.ProcessRequests(stat_requests, rh);
+    vector<StatRequest> stat_request;
+    RenderSettings render_settings;
+    TransportCatalogue catalogue;   
+     
+    JSONReader json_reader;
+    RequestHandler request_handler;
+        
+    json_reader = JSONReader(cin);    
+    json_reader.parse(catalogue, stat_request, render_settings);
+    
+    request_handler = RequestHandler();    
+    request_handler.execute_queries(catalogue, stat_request, render_settings);
+    transport_catalogue::detail::json::print(request_handler.get_document(), cout);
 }
