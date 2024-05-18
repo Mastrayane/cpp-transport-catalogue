7b960b26a31acbe41c4adcaa40918aff8d63b8a5
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-18 16:21:31 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -5,96 +5,70 @@
 #include <string>
 #include <vector>
 #include <variant>
-#include <variant>
 
-using namespace std::string_view_literals;
+namespace transport_catalogue {
+    namespace detail {
+        namespace json {
 
-namespace json {
+            class Node;
 
-	class Node;
-	// Сохраните объявления Dict и Array без изменения
-	using Dict = std::map<std::string, Node>;
-	using Array = std::vector<Node>;
+            using Dict = std::map<std::string, Node>;
+            using Array = std::vector<Node>;
 
-	// Эта ошибка должна выбрасываться при ошибках парсинга JSON
-	class ParsingError : public std::runtime_error {
-	public:
-		using runtime_error::runtime_error;
-	};
+            class ParsingError : public std::runtime_error {
+            public:
+                using runtime_error::runtime_error;
+            };
 
+            class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+            public:
 
-	class Node : private std::variant<std::nullptr_t, std::string, int, double, bool, Array, Dict> {
-	public:
+                using variant::variant;
+                using Value = variant;
 
-		using variant::variant;
+                bool IsNull() const;
+                bool IsInt() const;
+                bool IsDouble() const;
+                bool IsPureDouble() const;
+                bool IsBool() const;
+                bool IsString() const;
+                bool IsArray() const;
+                bool IsMap() const;
 
-		bool IsInt() const;
-		bool IsDouble() const;
-		bool IsPureDouble() const;
-		bool IsBool() const;
-		bool IsString() const;
-		bool IsNull() const;
-		bool IsArray() const;
-		bool IsMap() const;
+                const Array& AsArray() const;
+                const Dict& AsMap() const;
+                int AsInt() const;
+                double AsDouble() const;
+                bool AsBool() const;
+                const std::string& AsString() const;
 
-		int AsInt() const;
-		bool AsBool() const;
-		double AsDouble() const;
-		const std::string& AsString() const;
-		const Array& AsArray() const;
-		const Dict& AsMap() const;
+                const Value& GetValue() const;
 
-		const variant& GetValue() const;
+            };
 
-	};
+            bool operator==(const Node& lhs, const Node& rhs);
+            bool operator!=(const Node& lhs, const Node& rhs);
 
-	bool operator==(const Node& lhs, const Node& rhs);
-	bool operator!=(const Node& lhs, const Node& rhs);
+            class Document {
+            public:
+                Document() = default;
+                explicit Document(Node root);
+                const Node& get_root() const;
 
+            private:
+                Node root_;
+            };
 
-	class Document {
-	public:
-		explicit Document(Node root);
+            inline bool operator==(const Document& lhs, const Document& rhs) {
+                return lhs.get_root() == rhs.get_root();
+            }
+            inline bool operator!=(const Document& lhs, const Document& rhs) {
+                return !(lhs == rhs);
+            }
 
-		const Node& GetRoot() const;
+            Document load(std::istream& input);
+            void print(const Document& document, std::ostream& output);
 
-		bool operator==(const Document& rhs) const;
-		bool operator!=(const Document& rhs) const;
-
-	private:
-		Node root_;
-	};
-
-	Document Load(std::istream& input);
-
-	// Контекст вывода, хранит ссылку на поток вывода и текущий отсуп
-	struct PrintContext {
-		std::ostream& out;
-		int indent_step = 4;
-		int indent = 0;
-
-		void PrintIndent() const {
-			for (int i = 0; i < indent; ++i) {
-				out.put(' ');
-			}
-		}
-		// Возвращает новый контекст вывода с увеличенным смещением
-		PrintContext Indented() const {
-			return { out, indent_step, indent_step + indent };
-		}
-	};
-
-	void PrintValue(std::nullptr_t, const PrintContext& ctx);
-	void PrintValue(std::string value, const PrintContext& ctx);
-	void PrintValue(bool value, const PrintContext& ctx);
-	void PrintValue(Array array, const PrintContext& ctx);
-	void PrintValue(Dict dict, const PrintContext& ctx);
-	// Шаблон, подходящий для вывода double и int
-	template <typename Value>
-	void PrintValue(const Value& value, const PrintContext& ctx) {
-		ctx.out << value;
-	}
-	void PrintNode(const Node& node, const PrintContext& ctx);
-	void Print(const Document& doc, std::ostream& output);
-
-}  // namespace json
+        }//end namespace json
+    }//end namespace detail
+}//end namespace transport_catalogue
