8ab685207860511312c0f02b14608d9c9ac065b6
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-19 22:42:47 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,59 +1,59 @@
 #pragma once
+
+#include "geo.h"
+#include "domain.h"
+
+#include <iostream>
 #include <deque>
 #include <string>
-#include <vector>
-#include <iomanip>
-#include <iostream>
-#include <unordered_set>
 #include <unordered_map>
- 
-#include "domain.h"
- 
-using namespace domain;
- 
-namespace transport_catalogue {   
- 
-struct DistanceHasher {
-    std::hash<const void*> hasher;
-    
-    std::size_t operator()(const std::pair<const Stop*, const Stop*> pair_stops) const noexcept {
-        auto hash_1 = static_cast<const void*>(pair_stops.first);
-        auto hash_2 = static_cast<const void*>(pair_stops.second);
-        return hasher(hash_1) * 17 + hasher(hash_2);
-    }  
-};
-    
-typedef  std::unordered_map<std::string_view, Stop*> StopMap;
-typedef  std::unordered_map<std::string_view, Bus*> BusMap;
-typedef  std::unordered_map<std::pair<const Stop*, const  Stop*>, int, DistanceHasher> DistanceMap;
- 
-class TransportCatalogue {
-public:      
-    void add_bus(Bus&& bus);
-    void add_stop(Stop&& stop);
-    void add_distance(const std::vector<Distance>& distances);
-    
-    Bus* get_bus(std::string_view bus_name);
-    Stop* get_stop(std::string_view stop_name);
-    BusMap get_busname_to_bus() const;
-    StopMap get_stopname_to_stop() const;
-    
-    std::unordered_set<const Bus*> stop_get_uniq_buses(Stop* stop);    
-    std::unordered_set<const Stop*> get_uniq_stops(Bus* bus);
-    double get_length(Bus* bus);
-    
-    size_t get_distance_stop(const Stop* start, const Stop* finish);
-    size_t get_distance_to_bus(Bus* bus);
-    
-private:    
-    std::deque<Stop> stops;
-    StopMap stopname_to_stop;
-    
-    std::deque<Bus> buses;
-    BusMap busname_to_bus;
-    
-    DistanceMap distance_to_stop;
-    
-};
-    
-}//end namespace transport_catalogue
+#include <vector>
+#include <stdexcept>
+#include <optional>
+#include <unordered_set>
+#include <set>
+#include <map>
+
+namespace transport {
+
+	class Catalogue {
+	public:
+		struct StopDistancesHasher {
+			size_t operator()(const std::pair<const Stop*, const Stop*>& points) const {
+				size_t hash_first = std::hash<const void*>{}(points.first);
+				size_t hash_second = std::hash<const void*>{}(points.second);
+				return hash_first + hash_second * 37;
+			}
+		};
+
+		void AddStop(std::string_view stop_name, const geo::Coordinates coordinates);
+		void AddRoute(std::string_view bus_number, const std::vector<const Stop*>& stops, bool is_circle);
+		const Bus* FindRoute(std::string_view bus_number) const;
+		const Stop* FindStop(std::string_view stop_name) const;
+
+		size_t GetUniqueStopsCount(std::string_view bus_number) const;
+		/*
+		Извините, в request_handler.cpp уже есть метод GetBusStat,
+		который возвращает сразу все характеристики маршрута.
+		Данный метод GetBusStat использует метод GetUniqueStopsCount,
+		и перевод метода в приватную область делает его недоступным в class RequestHandler
+		даже в случае создания наследования RequestHandler от Catalogue или обратного.
+		Дублирование данного метода в этот класс, навереное не то, что вы от меня хотели.
+
+		Скорее всего я не правильно Вас понял. Уточните пожалуйста этот момент.
+
+		*/
+		void SetDistance(const Stop* from, const Stop* to, const int distance);
+		int GetDistance(const Stop* from, const Stop* to) const;
+		const std::map<std::string_view, const Bus*> GetSortedAllBuses() const;
+
+	private:
+
+		std::deque<Bus> all_buses_;
+		std::deque<Stop> all_stops_;
+		std::unordered_map<std::string_view, const Bus*> busname_to_bus_;
+		std::unordered_map<std::string_view, const Stop*> stopname_to_stop_;
+		std::unordered_map<std::pair<const Stop*, const Stop*>, int, StopDistancesHasher> stop_distances_;
+	};
+
+} // namespace transport
