f79856cf07ac79a3605c28d74e24ac406964fa80
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-05-19 22:33:21 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -3,72 +3,70 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
 #include <variant>
+#include <vector>
 
-namespace transport_catalogue {
-    namespace detail {
-        namespace json {
+namespace json {
 
-            class Node;
+	class Node;
+	using Dict = std::map<std::string, Node>;
+	using Array = std::vector<Node>;
 
-            using Dict = std::map<std::string, Node>;
-            using Array = std::vector<Node>;
+	class ParsingError : public std::runtime_error {
+	public:
+		using runtime_error::runtime_error;
+	};
 
-            class ParsingError : public std::runtime_error {
-            public:
-                using runtime_error::runtime_error;
-            };
+	class Node final
+		: private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+	public:
+		using variant::variant;
+		using Value = variant;
 
-            class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
-            public:
+		bool IsInt() const;
+		bool IsDouble() const;
+		bool IsPureDouble() const;
+		bool IsBool() const;
+		bool IsString() const;
+		bool IsNull() const;
+		bool IsArray() const;
+		bool IsDict() const;
 
-                using variant::variant;
-                using Value = variant;
+		int AsInt() const;
+		bool AsBool() const;
+		double AsDouble() const;
+		const std::string& AsString() const;
+		const Array& AsArray() const;
+		const Dict& AsMap() const;
 
-                bool IsNull() const;
-                bool IsInt() const;
-                bool IsDouble() const;
-                bool IsPureDouble() const;
-                bool IsBool() const;
-                bool IsString() const;
-                bool IsArray() const;
-                bool IsMap() const;
+		bool operator==(const Node& rhs) const;
 
-                const Array& AsArray() const;
-                const Dict& AsMap() const;
-                int AsInt() const;
-                double AsDouble() const;
-                bool AsBool() const;
-                const std::string& AsString() const;
+		const Value& GetValue() const;
 
-                const Value& GetValue() const;
+		Value& GetValue();
+	};
 
-            };
+	inline bool operator!=(const Node& lhs, const Node& rhs);
 
-            bool operator==(const Node& lhs, const Node& rhs);
-            bool operator!=(const Node& lhs, const Node& rhs);
+	class Document {
+	public:
+		explicit Document(Node root)
+			: root_(std::move(root)) {
+		}
 
-            class Document {
-            public:
-                Document() = default;
-                explicit Document(Node root);
-                const Node& get_root() const;
+		const Node& GetRoot() const {
+			return root_;
+		}
 
-            private:
-                Node root_;
-            };
+	private:
+		Node root_;
+	};
 
-            inline bool operator==(const Document& lhs, const Document& rhs) {
-                return lhs.get_root() == rhs.get_root();
-            }
-            inline bool operator!=(const Document& lhs, const Document& rhs) {
-                return !(lhs == rhs);
-            }
+	inline bool operator==(const Document& lhs, const Document& rhs);
+	inline bool operator!=(const Document& lhs, const Document& rhs);
 
-            Document load(std::istream& input);
-            void print(const Document& document, std::ostream& output);
+	Document Load(std::istream& input);
 
-        }//end namespace json
-    }//end namespace detail
-}//end namespace transport_catalogue
+	void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
